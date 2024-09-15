2ba744c4c0080a6cb1562575b547e0dcdadd6482
Michael J <30031129@cc.peralta.edu>
2024-09-15 13:33:08 -0700

    added print console

--- Table.cpp
+++ Table.cpp
@@ -5,5 +5,6 @@ using namespace std;
 
 int main(){
 
-    cout << left << setw(10) << "course" << right << "student";
+    cout << left << setw(10) << "course" << right << "student" << endl
+    << left << setw(10) << "C++" << 100 << endl << left << setw(10) << "JavaScript" << 50 << endl;
 }
